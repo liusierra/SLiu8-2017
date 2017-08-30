0c2a4c3f7549f5e559d7573e31389186a91f6706
liusierra <19liuy49@millshighsch.org>
2017-08-30 10:36:59 -0700

    8/30

---
+++ Project_2/src/Project2B.java
@@ -0,0 +1,11 @@
+//Sierra Liu
+// August 24,2017
+
+public class Project2B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,10 @@
+//Sierra Liu
+//August 23, 2017
+//Project 2 - the print out 
+public class Project_2 {
+	   public static void main(String[] args) {
+	System.out.println("HI,there");
+	System.out.println("Hello");
+	System.out.println("Hello2");
+	   }
+}
