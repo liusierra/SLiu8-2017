fec21a32085b5cd1dc04f345f2bd9a1247e91641
liusierra <19liuy49@millshighsch.org>
2018-03-14 09:51:54 -0700

    ch1

---
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -0,0 +1,46 @@
+package textExcel;
+
+// Update this file with your own code.
+
+public class Spreadsheet implements Grid
+{
+
+	@Override
+	public String processCommand(String command)
+	{
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+	@Override
+	public int getRows()
+	{
+		return 20;
+	}
+
+	@Override
+	public int getCols()
+	{
+		// TODO Auto-generated method stub
+		return 12;
+	}
+
+	@Override
+	public Cell getCell(Location loc)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGridText()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+     public String processCommand() {
+    	 return " ";}
+     
+     
+     }
---
+++ TextExcel/src/textExcel/SpreadsheetLocation.java
@@ -0,0 +1,27 @@
+package textExcel;
+
+//Update this file with your own code.
+
+public class SpreadsheetLocation implements Location
+{
+   String cellName= " ";
+    
+    public int getRow()
+    {
+    	mathparse. 
+        return -1;
+    }
+
+    public int getCol()
+    {
+    	
+        int colletter;
+		return ((int)(colletter=0);
+    }
+    
+    public SpreadsheetLocation(String cellName)
+    {
+        this cellName = cellName;
+    }
+
+}
